7a6c8dfbefcc963d5979be413034ce6b42abafb3
Nguyen Huy Khai <nguyenhuykhaipch@gmail.com>
2023-08-16 10:57:45 +0700

    Mern Auth - Lesson 2

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const workoutRoutes = require("./routes/workouts");
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require("./routes/user");
 
 // epress app
 const app = express();
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // routers
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 // connect to db
 mongoose
